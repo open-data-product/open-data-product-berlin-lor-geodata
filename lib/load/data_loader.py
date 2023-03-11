5e6abd61308b300878cf92d6406f8e97d42d8d39
Florian Schwanz <florian.schwanz@interoberlin.de>
2023-03-11 11:40:08 +0100

    chore: add data loader #1

---
+++ lib/load/data_loader.py
@@ -0,0 +1,49 @@
+import json
+import os
+
+from lib.tracking_decorator import TrackingDecorator
+
+
+@TrackingDecorator.track_time
+def load_data(source_path, results_path, clean=False, quiet=False):
+    # Iterate over files
+    for subdir, dirs, files in os.walk(source_path):
+        for file_name in files:
+            subdir = subdir.replace(f"{source_path}/", "")
+            target_file_name = get_target_file_name(file_name)
+
+            # Make results path
+            os.makedirs(os.path.join(results_path, subdir), exist_ok=True)
+
+            source_file_path = os.path.join(source_path, subdir, file_name)
+            results_file_path = os.path.join(results_path, subdir, target_file_name)
+
+            # Check if file needs to be copied
+            if clean or not os.path.exists(results_file_path):
+                with open(source_file_path, "r", encoding="utf-8") as geojson_file:
+                    geojson = json.load(geojson_file, strict=False)
+
+                with open(results_file_path, "w", encoding="utf-8") as geojson_file:
+                    json.dump(geojson, geojson_file, ensure_ascii=False)
+
+                    if not quiet:
+                        print(f"✓ Load {target_file_name}")
+            else:
+                print(f"✓ Already exists {target_file_name}")
+
+
+def get_target_file_name(source_file_name):
+    if source_file_name == "bezirksgrenzen.geojson":
+        return "berlin-lor-districts.geoson"
+    elif source_file_name == "lor_prognoseraeume.geojson":
+        return "berlin-lor-forecast-areas-until-2020.geojson"
+    elif source_file_name == "lor_prognoseraeume_2021.geojson":
+        return "berlin-lor-forecast-areas-from-2021.geojson"
+    elif source_file_name == "lor_bezirksregionen.geojson":
+        return "berlin-lor-district-regions-until-2020.geojson"
+    elif source_file_name == "lor_bezirksregionen_2021.geojson":
+        return "berlin-lor-district-regions-from-2021.geojson"
+    elif source_file_name == "lor_planungsraeume.geojson":
+        return "berlin-lor-planning-areas-until-2020.geojson"
+    elif source_file_name == "lor_planungsraeume_2021.geojson":
+        return "berlin-lor-planning-areas-from-2021.geojson"
