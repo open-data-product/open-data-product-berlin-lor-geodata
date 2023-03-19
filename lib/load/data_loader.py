a8362f606cd88996543bdfa57624dc8bce2db6d9
Florian Schwanz <florian.schwanz@interoberlin.de>
2023-03-19 22:04:03 +0100

    chore: update data loader #1

--- lib/load/data_loader.py
+++ lib/load/data_loader.py
@@ -1,5 +1,5 @@
-import json
 import os
+import shutil
 
 from lib.tracking_decorator import TrackingDecorator
 
@@ -9,23 +9,22 @@ def load_data(source_path, results_path, clean=False, quiet=False):
     # Iterate over files
     for subdir, dirs, files in os.walk(source_path):
         for file_name in files:
-            subdir = subdir.replace(f"{source_path}/", "")
+            _, file_extension = os.path.splitext(file_name)
 
-            # Make results path
-            os.makedirs(os.path.join(results_path, subdir), exist_ok=True)
+            if file_extension in [".geojson"]:
+                subdir = subdir.replace(f"{source_path}/", "")
 
-            source_file_path = os.path.join(source_path, subdir, file_name)
-            results_file_path = os.path.join(results_path, subdir, file_name)
+                # Make results path
+                os.makedirs(os.path.join(results_path, subdir), exist_ok=True)
 
-            # Check if file needs to be copied
-            if clean or not os.path.exists(results_file_path):
-                with open(source_file_path, "r", encoding="utf-8") as geojson_file:
-                    geojson = json.load(geojson_file, strict=False)
+                source_file_path = os.path.join(source_path, subdir, file_name)
+                results_file_path = os.path.join(results_path, subdir, file_name)
 
-                with open(results_file_path, "w", encoding="utf-8") as geojson_file:
-                    json.dump(geojson, geojson_file, ensure_ascii=False)
+                # Check if file needs to be copied
+                if clean or not os.path.exists(results_file_path):
+                    shutil.copyfile(source_file_path, results_file_path)
 
                     if not quiet:
                         print(f"✓ Load {file_name}")
-            else:
-                print(f"✓ Already exists {file_name}")
+                else:
+                    print(f"✓ Already exists {file_name}")
