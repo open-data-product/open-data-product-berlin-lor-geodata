83e23fc8666b1c1d98186813421099ca4bfbefdc
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-03-29 14:58:41 +0100

    chore: add click

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getopt
 import os
 import sys
+
+import click
 
 from config.data_product_manifest_loader import load_data_product_manifest
 from config.data_transformation_loader import load_data_transformation
@@ -8,7 +9,6 @@ from config.odps_loader import load_odps
 from documentation.data_product_canvas_generator import generate_data_product_canvas
 from documentation.data_product_manifest_updater import update_data_product_manifest
 from lib.extract.data_extractor import extract_data
-from lib.tracking_decorator import TrackingDecorator
 from lib.transform.data_bounding_box_converter import convert_bounding_box
 from lib.transform.data_combiner import combine_districts_into_city
 from lib.transform.data_lor_area_matcher import identify_lor_area_matches
@@ -21,32 +21,10 @@ file_path = os.path.realpath(__file__)
 script_path = os.path.dirname(file_path)
 
 
-@TrackingDecorator.track_time
-def main(argv):
-    # Set default values
-    clean = False
-    quiet = False
-
-    # Read command line arguments
-    try:
-        opts, args = getopt.getopt(argv, "hcq", ["help", "clean", "quiet"])
-    except getopt.GetoptError:
-        print("main.py --help --clean --quiet")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print("main.py")
-            print("--help                           show this help")
-            print(
-                "--clean                          clean intermediate results before start"
-            )
-            print("--quiet                          do not log outputs")
-            sys.exit()
-        elif opt in ("-c", "--clean"):
-            clean = True
-        elif opt in ("-q", "--quiet"):
-            quiet = True
-
+@click.command()
+@click.option("--clean", default=False, help="Regenerate results.")
+@click.option("--quiet", default=False, help="Do not log outputs.")
+def main(clean, quiet):
     data_path = os.path.join(script_path, "data")
     bronze_path = os.path.join(data_path, "01-bronze")
     silver_path = os.path.join(data_path, "02-silver")
